2d390e68b13d6a36129c3aeccbfe06b196a86409
Suneeporn85 <suneeporn.8543@gmail.com>
2021-08-19 00:09:43 +0700

    first commit

---
+++ lib/Data/productdata.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/Models/products.dart';
+
+final Product pixelStand = Product(
+  imagePath: "assets/images/cactus1.webp",
+  name: "กระถาง 8 เหลี่ยม",
+  description: "กระถางแคคตัส 8 เหลี่ยม",
+  buttonText: "Learn more",
+  price: 20,
+  backgroundColor: Color(0xFFE8F5E9),
+  productInfo: "กระถาง 8 เหลี่ยม 3.5 นิ้ว"
+      "กระถางแคคตัส กระถางกระบองเพชร กระถางพลาสติก กระถางต้นไม้.",
+);
+
+final Product dayDreamView = Product(
+  imagePath: "assets/images/cactus2.jpg",
+  name: "กระถางพลาสติก",
+  description: "กระถางทรงกลมสูง ",
+  buttonText: "Learn more",
+  price: 20,
+  backgroundColor: Color(0xFFC8E6C9),
+  productInfo: "กว้าง 2 นิ้ว "
+      "มี 4 สีให้เลือก สีขาว สีดำ สีอิฐ สีเขียว ใช้สำหรับปลูกแคคตัส.",
+);
+
+final Product pixel = Product(
+  imagePath: "assets/images/cactus3.jpg",
+  name: "กระถางไม้โขด",
+  description: "กระถางไม้โขด.",
+  buttonText: "Learn more",
+  price: 25,
+  backgroundColor: Color(0xFFE8F5E9),
+  productInfo: "กระถางแคคตัส กระถางไม้โขด "
+      "กระถางกระบองเพชร กระถางไม้มงคล ขนาด 2 นิ้ว.",
+);
+
+final Product stadia = Product(
+  imagePath: "assets/images/cactus5.jpg",
+  name: "กระถางดินเผา",
+  description: "กระถางดินเผา",
+  buttonText: "Learn more",
+  backgroundColor: Color(0xFFC8E6C9),
+  price: 30,
+  productInfo: "กระถางดินเผา กระถางแคคตัส "
+      "กระบองเพชร บอนไซ ไม้อวบน้ำ ขนาด 3 นิ้ว.",
+);
